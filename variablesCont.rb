bec7c0e97c20eb559d7fd6311c60110c871ae4f3
wbschlosser <wesleysemail37@yahoo.com>
2015-01-16 23:09:45 -0600

    practice excercises 1-7

---
+++ variablesCont.rb
@@ -0,0 +1,16 @@
+name = 'Wes Schlosser'
+age = 24
+height = 70.0
+centimeters = height * 2.54
+weight = 145.0
+kilograms = weight * 0.45
+eyes = 'blue'
+teeth = 'off-white'
+hair = 'dirty blonde'
+
+puts "Lets talk about #{name}"
+puts "He's #{age} years old is #{centimeters} inches tall and weighs #{kilograms} pounds."
+puts "his #{eyes} eyes are cool and, due to not flossing enough, has slightly #{teeth} teeth."
+puts "By the way his hair is #{hair}."
+
+puts "If you add his #{height}, #{age}, and #{weight} together you get #{height + age + weight}."
