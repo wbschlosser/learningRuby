bd1e98cba35806c3e34b9c81c0a5dda66a7fb831
wbschlosser <wesleysemail37@yahoo.com>
2015-01-31 16:24:13 -0600

    debugging tips

--- ex31thru40/e33whileLoops.rb
+++ ex31thru40/e33whileLoops.rb
@@ -1,18 +1,21 @@
 #obviously executes code in its block until its condition is false
-
+#methods must be defined before they are used
+def loopTime(a,b,c)
+	while a < b
+		puts "At the top a is #{a}"
+		c.push(a)
+		a+= 1
+		puts "numbers now ", c
+		puts "at the bottom i is #{a}"
+	end
+end
 i = 0
 puts "input a number: "
 input = $stdin.gets.chomp.to_i
 numbers = []
+loopTime(i,input,numbers)
 
-	while i < input
-		puts "At the top i is #{i}"
-		numbers.push(i)
-		i+= 1
-		puts "numbers now ", numbers
-		puts "at the bottom i is #{i}"
-	end
-
-puts "the numbers: "
-numbers.each{|num| puts num}
-
+puts "the numbers, after the function is run!!!!!: "
+numbers.each do |num|
+	puts num
+end
---
+++ ex31thru40/e36designDebug.rb
@@ -0,0 +1,24 @@
+#Rules for if statments
+# 1.If statements should have an else
+# 2.If the else should never run because it doesn't make sense
+#	use a 'die' function to print an error message and die (see e35)
+# 3. Never nest them more than 2 deep, always try for 1 deep only
+# 4. Treat them like paragraphs, if-elsif-else with blank lines
+#	before and after them
+# 5. Boolean tests should be simple. Move complex calculations to
+#	variables earlier in the function. Use good naming
+###################################################
+#Rules for Loops
+#1. use while loops only to loop forever, this only applies to ruby
+#2. use for loops for all other looping, especially if there is a
+#	fixed or limited number of things to loop over
+###################################################
+#Debugging tips
+# 1. Don't use one in ruby. They are overkill apparently
+# 2. The best way is to use put to print out variables at points in the
+#	program to see where they go wrong.
+# 3. Code a little, run a little, fix a litte. Dont write big programs
+#	all at once
+##################################################
+
+
