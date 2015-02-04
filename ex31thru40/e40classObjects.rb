f31bef0ada66fe5387079b05c4f74edecee34a6c
wbschlosser <wesleysemail37@yahoo.com>
2015-02-03 21:43:14 -0600

    had TWO car crashes, this is a placeholder for later

---
+++ ex31thru40/e40classObjects.rb
@@ -0,0 +1,9 @@
+#modules are like hashes
+mystuff = {'apple' => "I AM APPLES!"}
+#this doesn't work the other way around
+puts mystuff['apple']
+#"Gets x from y"
+#"gets I AM APPLES from apple"
+
+#you can access the functions and variables in a module with the . operator
+
